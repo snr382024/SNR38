c56bd721b84e29d25b4c11c5a4c225ae6430f9db
connorjohnson6 <connor.johnson1@marist.edu>
2024-04-12 00:58:20 -0400

    Working Template 1

--- SNR38Site/src/components/PageTemplate1/TemplateOne.tsx
+++ SNR38Site/src/components/PageTemplate1/TemplateOne.tsx
@@ -1,6 +1,6 @@
 //TemplateOnce.tsx
 
-import React from "react";
+import React, { useState } from "react";
 import "./templateone.scss";
 
 import LongImage from "./media/LongImage.png";
@@ -9,10 +9,16 @@ import LongImage3 from "./media/LongImage3.png";
 import LongImage4 from "./media/LongImage4.png";
 
 import ShortImage from "./media/ShortImage.png";
-
 import Video1 from "./media/video1.mp4";
 
 const TemplateOne: React.FC = () => {
+  const [isFlipped, setIsFlipped] = useState([false, false, false, false]);
+
+  const handleFlip = (index: number) => {
+    const flippedStates = [...isFlipped];
+    flippedStates[index] = !flippedStates[index];
+    setIsFlipped(flippedStates);
+  };
 
   return (
     <div className="template-one-container">
@@ -21,13 +27,31 @@ const TemplateOne: React.FC = () => {
           <div className="square">
             <img src={ShortImage} alt="Square Image" />
           </div>
-          <div className="rectangle">
-            <img src={LongImage} alt="Rectangle Image" />
+          <div className="rectangle" onClick={() => handleFlip(0)}>
+            <div className={`flip-container ${isFlipped[0] ? "flipped" : ""}`}>
+              <div className="flipper">
+                <div className="front">
+                  <img src={LongImage} alt="Front of the Dress" />
+                </div>
+                <div className="back">
+                  <img src={LongImage2} alt="Back of the Dress" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="column-type2">
-          <div className="rectangle">
-            <img src={LongImage2} alt="Rectangle Image" />
+          <div className="rectangle" onClick={() => handleFlip(1)}>
+            <div className={`flip-container ${isFlipped[1] ? "flipped" : ""}`}>
+              <div className="flipper">
+                <div className="front">
+                  <img src={LongImage} alt="Front of the Dress" />
+                </div>
+                <div className="back">
+                  <img src={LongImage2} alt="Back of the Dress" />
+                </div>
+              </div>
+            </div>
           </div>
           <div className="square">
             <img src={ShortImage} alt="Square Image" />
@@ -37,13 +61,31 @@ const TemplateOne: React.FC = () => {
           <div className="square">
             <img src={ShortImage} alt="Square Image" />
           </div>
-          <div className="rectangle">
-            <img src={LongImage3} alt="Rectangle Image" />
+          <div className="rectangle" onClick={() => handleFlip(2)}>
+            <div className={`flip-container ${isFlipped[2] ? "flipped" : ""}`}>
+              <div className="flipper">
+                <div className="front">
+                  <img src={LongImage} alt="Front of the Dress" />
+                </div>
+                <div className="back">
+                  <img src={LongImage2} alt="Back of the Dress" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="column-type2">
-          <div className="rectangle">
-            <img src={LongImage4} alt="Rectangle Image" />
+          <div className="rectangle" onClick={() => handleFlip(3)}>
+            <div className={`flip-container ${isFlipped[3] ? "flipped" : ""}`}>
+              <div className="flipper">
+                <div className="front">
+                  <img src={LongImage} alt="Front of the Dress" />
+                </div>
+                <div className="back">
+                  <img src={LongImage2} alt="Back of the Dress" />
+                </div>
+              </div>
+            </div>
           </div>
           <div className="square">
             <img src={ShortImage} alt="Square Image" />
